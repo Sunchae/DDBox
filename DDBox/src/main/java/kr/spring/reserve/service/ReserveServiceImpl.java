423d4642055e392cefef03771dead35d44f727eb
SAMSUNG <SAMSUNG@BOOK-VNB3OQSTDR>
2024-01-22 22:47:00 +0900

    [인화]service 수정

--- DDBox/src/main/java/kr/spring/reserve/service/ReserveServiceImpl.java
+++ DDBox/src/main/java/kr/spring/reserve/service/ReserveServiceImpl.java
@@ -3,10 +3,15 @@ package kr.spring.reserve.service;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import kr.spring.reserve.vo.ScreenVO;
 import kr.spring.reserve.vo.ShowVO;
 import kr.spring.reserve.vo.TicketVO;
 
+@Service
+@Transactional
 public class ReserveServiceImpl implements ReserveService{
 
 	@Override
